44589adb73a7c396c70bca0b5f3816ad03a63069
Scott - Jeonghoon Seo <cless00@gmail.com>
2025-03-13 16:01:51 +0900

    add page for new place

--- frontend/app/routes/NewPlace.tsx
+++ frontend/app/routes/NewPlace.tsx
@@ -1,25 +1,88 @@
 import { Input } from "~/shared/FormElements/Input";
 import "./NewPlace.css";
-import {
-  VALIDATOR_MAXLENGTH,
-  VALIDATOR_MINLENGTH,
-  VALIDATOR_REQUIRE,
-} from "~/util/validators";
-import { useCallback } from "react";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "~/util/validators";
+import { useCallback, useReducer } from "react";
+import Button from "~/shared/FormElements/Button";
+
+type InputType = {
+  value: string | undefined;
+  isValid: boolean;
+};
+type StateType = {
+  inputs: {
+    [title: string]: InputType;
+    description: InputType;
+  };
+  isValid: boolean;
+};
+type ActionType = {
+  type: "INPUT_CHANGE";
+  inputId: string;
+  isValid: boolean;
+  value: string | undefined;
+};
+
+const formReducer = (state: StateType, action: ActionType): StateType => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: {
+            value: action.value,
+            isValid: action.isValid,
+          },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
 
 export const NewPlace = () => {
-  const titleInputHandler = useCallback(
-    (id: string, value: string | undefined, isValid: boolean) => {},
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  const inputHandler = useCallback(
+    (id: string, value: string | undefined, isValid: boolean) => {
+      dispatch({
+        type: "INPUT_CHANGE",
+        value: value,
+        isValid: isValid,
+        inputId: id,
+      });
+    },
     [],
   );
 
-  const descriptionInputHandler = useCallback(
-    (id: string, value: string | undefined, isValid: boolean) => {},
-    [],
-  );
-
+  const placeAddHandler = (event: any) => {
+    // todo: send to backend
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeAddHandler}>
       <Input
         id="title"
         element="input"
@@ -27,16 +90,28 @@ export const NewPlace = () => {
         label="Title"
         errorText="Please enter a valid title."
         validators={[VALIDATOR_REQUIRE()]}
-        onInput={titleInputHandler}
+        onInput={inputHandler}
       />
       <Input
         id="description"
         type="text"
         label="Description"
-        errorText="Please enter a valid description."
+        errorText="Please enter a valid description. (at least 5 characters)"
         validators={[VALIDATOR_MINLENGTH(5)]}
-        onInput={descriptionInputHandler}
+        onInput={inputHandler}
       />
+      <Input
+        id="address"
+        element="input"
+        type="text"
+        label="Address"
+        errorText="Please enter a valid address."
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        Add place
+      </Button>
     </form>
   );
 };
